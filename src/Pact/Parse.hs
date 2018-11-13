29a3cfcb72a81b40253df2bb0d3a6a5091841a6a
gyoung <652105072@qq.com>
2018-11-13 11:19:02 +0800

    add ^ separator

--- src/Pact/Parse.hs
+++ src/Pact/Parse.hs
@@ -50,6 +50,7 @@ expr = do
       separator t s = symbol t >> (ESeparator . SeparatorExp s <$> inf)
   msum
     [ TF.try (ELiteral <$> (LiteralExp <$> token number <*> inf)) <?> "number"
+    , separator "^" SCaret
     , ELiteral <$> (LiteralExp . LString <$> stringLiteral <*> inf) <?> "string"
     , ELiteral <$> (LiteralExp . LString <$> (symbolic '\'' >> ident style) <*> inf) <?> "symbol"
     , ELiteral <$> (LiteralExp <$> bool <*> inf) <?> "bool"
@@ -59,7 +60,7 @@ expr = do
     , EList <$> (ListExp <$> brackets (many expr) <*> pure Brackets <*> inf) <?> "{list}"
     , separator ":=" ColonEquals
     , separator ":" Colon
-    , separator "," Comma
+    , separator "," Comma   
     ]
 {-# INLINE expr #-}
 
