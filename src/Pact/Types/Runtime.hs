b4a87010626e815aa74350e021f5e54e31a4ef9b
gyoung <652105072@qq.com>
2018-11-28 14:56:45 +0800

    fixed bug

--- src/Pact/Types/Runtime.hs
+++ src/Pact/Types/Runtime.hs
@@ -264,7 +264,7 @@ instance Semigroup a => Semigroup (Eval e a) where
 
 instance Monoid a => Monoid (Eval e a) where
   mempty = pure mempty
-  mappend = (<>)
+  mappend = undefined
 
 -- | "Production" runEval throws exceptions, meaning the state can be lost,
 -- which is useful for reporting stack traces in the REPL.
