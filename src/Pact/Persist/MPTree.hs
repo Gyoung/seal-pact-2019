026a0ebf9ca781d4992368541577301388227c26
gyoung <652105072@qq.com>
2018-12-25 14:39:42 +0800

    delete PValue

--- src/Pact/Persist/MPTree.hs
+++ src/Pact/Persist/MPTree.hs
@@ -10,35 +10,27 @@
 
 module Pact.Persist.MPTree
   (
-    PValue (..),
     Tbl(..),tbl,
     Tables(..),tbls,
     Db(..),dataTables,txTables,
-    -- committed,tblType,stateRootTree,stateRootTable,
     rootMPDB,tableStateRoot,
     MPtreeDb(..),temp,
     initMPtreeDb,_test,
     persister
   ) where
 
--- import qualified Data.Map.Strict as M
 import Control.Lens hiding (op)
 import Data.Aeson
 import Control.Monad.Reader ()
 import Control.Monad.State
 import Data.Default
-import Data.Typeable
+-- import Data.Typeable
 import Data.Semigroup (Semigroup)
 
 import Pact.Persist hiding (compileQuery)
 
 import           Seal.DB.MerklePatricia
--- import           Seal.DB.MerklePatricia.Utils
--- import           Seal.DB.MerklePatricia.Internal
--- import qualified Data.NibbleString                              as N
 import           Pos.DB.Rocks.Functions                
--- import           Pos.Binary.Class (Bi (..), serialize',decodeFull')
--- import           Pos.DB.Rocks.Types
 import           Pos.DB.Rocks.Types (DB (..))
 import           Seal.DB.MerklePatricia.Utils
 import           Universum (MonadFail)
@@ -48,16 +40,11 @@ import qualified Data.ByteString.Lazy as BSL
 import qualified Pos.Util.Modifier as MM
 import Data.Text.Encoding
 import qualified Data.Text as T
--- import           Data.Hashable (Hashable)
 
--- import Universum((<>))
-
-data PValue = forall a . PactValue a => PValue a
-instance Show PValue where show (PValue a) = show a
 
 --MM.Modifyer 保存某张表的修改  
 data Tbl k = Tbl {
-  _tbl :: MM.MapModifier k PValue,
+  _tbl :: MM.MapModifier k B.ByteString,
   _tableStateRoot :: StateRoot
   } deriving (Show)
 makeLenses ''Tbl
@@ -80,12 +67,10 @@ tblType DataTable {} = dataTables
 tblType TxTable {} = txTables --todo 是否会有影响
 
 data MPtreeDb = MPtreeDb {
-  -- _committed :: !Db,
   _temp :: !Db,
   _rootMPDB :: MPDB           
   }
 makeLenses ''MPtreeDb
--- instance Default MPtreeDb where def = MPtreeDb def emptyTriePtr
 
 initMPtreeDb :: IO MPtreeDb
 initMPtreeDb = do
@@ -93,10 +78,6 @@ initMPtreeDb = do
   let rdb' = MPDB {rdb=db,stateRoot=emptyTriePtr}
   initializeBlank rdb'
   return $ MPtreeDb def rdb'
-
--- overM :: s -> Lens' s a -> (a -> IO a) -> IO s
--- overM s l f = f (view l s) >>= \a -> return (set l a s)
--- {-# INLINE overM #-}
 
 persister :: Persister MPtreeDb
 persister = Persister {
@@ -109,9 +90,11 @@ persister = Persister {
   ,
   rollbackTx = \s -> rollbackTx_ s
   ,
-  queryKeys = \t kq s -> (s,) . map fst <$> qry t kq s
+  -- queryKeys = \t kq s -> (s,) . map fst <$> qry t kq s
+  queryKeys = \t kq s -> queryKeys_ t kq s
   ,
-  query = \t kq s -> fmap (s,) $ qry t kq s >>= mapM (\(k,v) -> (k,) <$> conv v)
+  -- query = \t kq s -> fmap (s,) $ qry t kq s >>= mapM (\(k,v) -> (k,) <$> conv v)
+  query = \t kq s -> query_ t kq s
   ,
   readValue = \t k s -> readValue_ t k s
   ,
@@ -179,7 +162,7 @@ readValue_ t k s = do
           --从mptree中读取 
           Nothing     -> throwDbError $ "readValue: no such table: " ++ show t
           Just table  -> do
-            let valueGetter :: PactKey k => k -> IO (Maybe PValue)
+            let valueGetter :: PactKey k => k -> IO (Maybe B.ByteString)
                 valueGetter key = do
                   --将k转换成mpkey
                   let mpkey = pactKey2MPKey key
@@ -188,13 +171,14 @@ readValue_ t k s = do
                   let mpdb = MPDB {rdb=(rdb tMpdb),stateRoot=(_tableStateRoot table)}
                   --获取对应value
                   val <- getKeyVal mpdb mpkey
-                  return $ mpValToPValue val
+                  return val
             MM.lookupM valueGetter k (_tbl table)
-  v <- traverse conv $ pv
+  -- ByteString to PactValue 
+  let v = mpValToPv mpv          
   return $ (s, v)
 
+--writeValue MM中没有表，判断mptree中是否有，有的话创建一个MM,再写入MM
 writeValue_ :: (PactKey k, PactValue v) => Table k -> WriteType -> k -> v -> MPtreeDb -> IO (MPtreeDb,())
---todo:writeValue MM中没有表，判断mptree中是否有，有的话创建一个MM,再写入MM
 writeValue_ t wt k v s = do
   let tables = view (temp . tblType t . tbls) s
   let  baseGetter :: PactKey k => Table k ->  IO (Maybe (Tbl k))
@@ -210,7 +194,7 @@ writeValue_ t wt k v s = do
     --从mptree中读取 
     Nothing     -> throwDbError $ "readValue: no such table: " ++ show t
     Just table  -> do
-      let valueGetter :: PactKey k => k -> IO (Maybe PValue)
+      let valueGetter :: PactKey k => k -> IO (Maybe B.ByteString)
           valueGetter key = do
             --将k转换成mpkey
             let mpkey = pactKey2MPKey key
@@ -219,20 +203,27 @@ writeValue_ t wt k v s = do
             let mpdb = MPDB {rdb=(rdb tMpdb),stateRoot=(_tableStateRoot table)}
             --获取对应value
             val <- getKeyVal mpdb mpkey
-            return $ mpValToPValue val
+            return val
       mpv <- MM.lookupM valueGetter k (_tbl table)
       ntbl <- case (mpv,wt) of 
                (Just _,Insert) -> throwDbError $ "Insert: value already at key: " ++ show k
                (Nothing,Update) -> throwDbError $ "Update: no value at key: " ++ show k
-               _ -> return $ MM.insert k (PValue v) (_tbl table)
+               _ -> return $ MM.insert k (asByteString v) (_tbl table)
       return $ set tbl ntbl table
   --更新MM中tables
   let newTables = MM.insert t tb tables
   let ns = set (temp . tblType t . tbls) newTables s
   return $ (ns,())   
 
-mpValToPValue :: Maybe MPVal -> Maybe PValue
-mpValToPValue = fmap(\v -> PValue $ decodeUtf8 v)
+queryKeys_ :: Table k -> Maybe (KeyQuery k) -> MPtreeDb -> IO (MPtreeDb,[k])
+queryKeys_ _ _ _ = undefined
+
+query_ :: Table k -> Maybe (KeyQuery k) -> MPtreeDb -> IO (MPtreeDb,[(k,v)])
+query_ _ _ _ = undefined
+
+mpValToPv :: (PactValue v) => Maybe B.ByteString -> Maybe v
+mpValToPv (Just v)  = decode . BSL.fromStrict $ v
+mpValToPv Nothing = Nothing
 
 mpValToTbl :: PactKey k => Maybe MPVal -> Maybe (Tbl k)
 mpValToTbl = fmap(\p -> Tbl {_tbl=mempty, _tableStateRoot=StateRoot p})
@@ -246,12 +237,6 @@ tableKey2MPKey (TxTable t) = bytesToNibbleString $ sanitize t
 
 sanitize :: TableId -> B.ByteString
 sanitize (TableId t) = encodeUtf8 t
-
--- tablesSchema :: [(Table DataKey,Tbl DataKey)]
--- tablesSchema = []
-
--- tableSchema :: [(DataKey,PValue)]
--- tableSchema = []
 
 --将tables插入mptree
 setMptreeTables :: [(Table DataKey,Tbl DataKey)] -> MPDB -> IO MPDB
@@ -268,11 +253,10 @@ setMptreeTables [] mpdb = return mpdb
 
 
 -- 将modifyer中的值插入table mptree
-setMPtreeValues :: [(DataKey,PValue)] -> MPDB -> IO MPDB
+setMPtreeValues :: [(DataKey,B.ByteString)] -> MPDB -> IO MPDB
 setMPtreeValues ((k,v):xs) mpdb = do
   let mpKey = dataKey2MPKey k
-  let mpVal = pValue2MPVal v
-  rs <- setMPtreeValue mpKey mpVal mpdb
+  rs <- setMPtreeValue mpKey v mpdb
   --更新stateroot
   setMPtreeValues xs rs
 setMPtreeValues [] mpdb = return mpdb  
@@ -280,9 +264,9 @@ setMPtreeValues [] mpdb = return mpdb
 --Text -ByteString
 dataKey2MPKey :: DataKey -> MPKey
 dataKey2MPKey (DataKey k) = bytesToNibbleString $ encodeUtf8 k
---还是有双引号问题 用Json?
-pValue2MPVal :: PValue -> MPVal
-pValue2MPVal (PValue p) = BSL.toStrict $ encode p
+
+asByteString :: ToJSON v => v -> B.ByteString
+asByteString = BSL.toStrict . encode
 
 mpdb2MPval :: MPDB -> MPVal
 mpdb2MPval (MPDB _ (StateRoot sr)) = sr
@@ -323,21 +307,13 @@ setMPtreeValue k v mpdb = do
 --     conj OR = (||)
 -- {-# INLINE compileQuery #-}
 
-qry :: Table k -> Maybe (KeyQuery k) -> MPtreeDb -> IO [(k,PValue)]
+-- qry :: Table k -> Maybe (KeyQuery k) -> MPtreeDb -> IO [(k,PValue)]
 -- qry t kq s = case firstOf (temp . tblType t . tbls . ix t . tbl) s of
 --   Nothing -> throwDbError $ "query: no such table: " ++ show t
 --   Just m -> return $ filter (compileQuery kq . fst) $ M.toList m
-qry _ _ _ = undefined
+-- qry _ _ _ = undefined
 
-{-# INLINE qry #-}
-
-
-conv :: PactValue v => PValue -> IO v
-conv (PValue v) = case cast v of
-  Nothing -> throwDbError $ "Failed to reify DB value: " ++ show v
-  Just s -> return s
-{-# INLINE conv #-}
-
+-- {-# INLINE qry #-}
 
 _test :: IO ()
 _test = do
@@ -355,20 +331,20 @@ _test = do
     run $ createTable p dt
 
     -- run $ createTable p tt
-    -- run $ commitTx p
-    -- run $ beginTx p True
+    run $ commitTx p
+    run $ beginTx p True
     run $ writeValue p dt Insert "stuff" (T.pack "hello")
     run $ writeValue p dt Insert "tough" (String "goodbye")
 
     -- run $ writeValue p tt Write 1 (String "txy goodness")
     -- run $ writeValue p tt Insert 2 (String "txalicious")
     run $ commitTx p
-    -- run $ writeValue p dt Insert "stuff1" (String "hello")
+    run $ writeValue p dt Insert "stuff1" (String "hello")
     -- run $ writeValue p dt Insert "stuff1" (String "hello")
 
     -- run $ createTable p dt
 
-    -- run (readValue p dt "tough") >>= (liftIO . (print :: Maybe Value -> IO ()))
+    run (readValue p dt "tough") >>= (liftIO . (print :: Maybe Text -> IO ()))
     run (readValue p dt "stuff") >>= (liftIO . (print :: Maybe Value -> IO ()))
 
     -- run (query p dt (Just (KQKey KEQ "stuff"))) >>=
